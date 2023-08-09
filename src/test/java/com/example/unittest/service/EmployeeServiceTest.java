f774000e4f344449844b242ebc6be5bb38dfcf11
Frank Zou <frankbearzou@gmail.com>
2023-08-09 09:32:58 -0400

    test save employee service

--- src/test/java/com/example/unittest/service/EmployeeServiceTest.java
+++ src/test/java/com/example/unittest/service/EmployeeServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.unittest.service;
 
+import java.util.Optional;
+
+import com.example.unittest.entity.Employee;
 import com.example.unittest.repository.EmployeeRepository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EmployeeServiceTest {
@@ -19,9 +24,26 @@ class EmployeeServiceTest {
     @InjectMocks
     private EmployeeService employeeService;
 
+    private Employee employee;
+
+    @BeforeEach
+    public void setupEmployee() {
+        employee = new Employee();
+        employee.setFirstName("John");
+        employee.setLastName("Doe");
+        employee.setEmail("john.doe@abc.com");
+    }
+
     @Test
-    public void test() {
-        assertThat(employeeService).isNotNull();
-        assertThat(employeeRepository).isNotNull();
+    public void givenEmployee_whenSaveEmployee_thenReturnSavedEmployee() {
+        given(employeeRepository.findByEmail(anyString())).willReturn(Optional.empty());
+        given(employeeRepository.save(employee)).willReturn(employee);
+
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+
+        then(employeeRepository).should().findByEmail(anyString());
+        then(employeeRepository).should().save(employee);
+
+        assertThat(savedEmployee).isNotNull();
     }
 }
